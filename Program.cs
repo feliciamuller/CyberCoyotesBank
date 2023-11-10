7f9f802726e9015792da7695f101bb814ea8f15c
Felicia Müller <felicia-muller@hotmail.com>
2023-11-10 14:19:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CyberCoyotesBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
