4457d5e22c90f0fde43dfcfcf228f7987a7abcf3
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-11-10 15:09:02 +0100

    simple login system. will improve later when User class exists

---
+++ LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberCoyotesBank
+{
+    static internal class LoginManager
+    {
+        static public void LoginMenu(string username, string password)
+        {
+            //TODO Replace with users
+            List<string> list = new List<string>() { "Admin", "User" };
+
+
+            //TODO Replace with list.find("username") and check for password in user
+            if (list.Contains(username) && password == "123")
+            {
+                Console.WriteLine("Welcome");
+                Menu.PrintMainMenu();
+            }
+            else
+            {
+                Console.WriteLine("TryAgain");
+            }
+
+        }
+
+    }
+}
