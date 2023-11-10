07ee5a9c03fa41f4c98444300646147426944023
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-11-10 15:08:16 +0100

    Quick and simple menu to test

---
+++ Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CyberCoyotesBank
+{
+    static class Menu
+    {
+
+        static public void PrintLogin()
+        {
+            Console.WriteLine("Username:");
+           string usernameInput = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            string passwordInput = Console.ReadLine();
+
+            LoginManager.LoginMenu(usernameInput, passwordInput);
+        }
+
+        static public void PrintMainMenu()
+        {
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-----------BANK-------------------");
+                Console.WriteLine("What do you want to do");
+                Console.WriteLine();
+                Console.WriteLine("1. open bankaccount\n" +
+                    "2. create new bankaccont\n" +
+                    "0. Logout");
+
+                int input = Int32.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Open bankaccount not implemented");
+                        Console.WriteLine("Press any key to continue!");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Create bankaccont not implemented");
+                        Console.WriteLine("Press any key to continue!");
+                        Console.ReadKey();
+                        break;
+                    case 0:
+                        Console.WriteLine("Logout not implemented. you are stuck here");
+                        Console.WriteLine("Press any key to continue!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+            
+
+
+
+
+
+        }
+
+    }
+}
