72d4b1ba48ba19175c9a6a97d4c24e1921fc0f0d
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-11-12 12:04:02 +0100

    new Login method that returns bool Added check password for active user Added comments

--- LoginManager.cs
+++ LoginManager.cs
@@ -8,23 +8,45 @@ namespace CyberCoyotesBank
 {
     static internal class LoginManager
     {
-        static public void LoginMenu(string username, string password)
+
+        //Holds curret user logged in 
+        static User activeUser;
+
+        //checks list of users to find if username exists.
+        //if found in list check if username and password is correct and return true/false
+        static public bool Login(string username, string password)
         {
-            //TODO Replace with users
-            List<string> list = new List<string>() { "Admin", "User" };
+            //TODO Move list of users to better place
+            List<User> users = new List<User>() { new User("admin", "admin"), new User("test", "123") };
 
+            User user = users.Find(x => x._username == username);
 
-            //TODO Replace with list.find("username") and check for password in user
-            if (list.Contains(username) && password == "123")
+            if (user == null) { return false; }
+            if (user._username == username && user._password == password)
             {
-                Console.WriteLine("Welcome");
-                Menu.PrintMainMenu();
+                activeUser = user;
+                return true;
             }
             else
             {
-                Console.WriteLine("TryAgain");
+                return false;
             }
 
+        }
+
+        //Used to check password of active user
+        //can be used for password checks on actions with extra security
+        //example: enter password before transfering money
+        static public bool Password(string password)
+        {
+            if (password == activeUser._password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
