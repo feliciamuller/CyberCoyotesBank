afd190c84f650c5ab7a935319a86b855da63dac8
jonnaagus <jonnaagustafssoon@hotmail.com>
2023-11-14 15:10:26 +0100

    har lagt in loggan

--- Program.cs
+++ Program.cs
@@ -6,22 +6,6 @@ namespace CyberCoyotesBank
     {
         static void Main(string[] args)
         {
-            //string name = Console.ReadLine();
-
-            //string userName = Console.ReadLine();
-
-            
-
-            Account account = new Account();
-            account.CreateAccount("Sparkonto", 100000); ;
-            //account.CheckBalance();
-            account.CreateAccount("Lönekonto", 100);
-            //account.CheckBalance();
-            account.CreateAccount("Räntekonto", 2000);
-            account.CheckBalance();
-            account.TransferMoney();
-            account.CheckBalance();
-
             Menu.PrintLogin();
         }
     }
