3251b6ccc5ff55abd86ec645fb7d26ad235db024
Tobias Söderqvist <saibot155@outlook.com>
2023-11-14 13:32:48 +0100

    Merged Master Into Tobias Branch

---
+++ User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberCoyotesBank
+{
+
+    internal class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int ID { get; set; }
+
+        public User(string userName, string password, int id)
+        {
+            UserName = userName;
+            Password = password;
+            ID = id; 
+        }
+
+        public void CreateAccount()
+        {
+            Console.WriteLine("Ett konto är skapat för användare: " + UserName);
+        }
+
+        public void Loan()
+        {
+            Console.WriteLine("Lånet är beviljat för användare: " + UserName);
+        }
+    }
+}
