9a7dcbe4bd47de1a136e130f92b34cd8813ea25d
fredrik nellbeck <Foggel@live.se>
2023-11-16 13:30:47 +0100

    Update Account

--- Account.cs
+++ Account.cs
@@ -34,7 +34,8 @@ namespace CyberCoyotesBank
         string Currency { get; set; }
 
         List<Account> accountLists = new List<Account>();
-        List<Account> accountNames = new List<Account>();
+        List<string> accountHistory = new List<string>();
+
         public List<object> userList { get; set ; }
 
         public Account()
@@ -65,42 +66,79 @@ namespace CyberCoyotesBank
             }
 
         }
-
         public void TransferMoney() 
         {
+            Console.WriteLine("Your accounts:");
             foreach (var i in accountLists)
             {
-
                 Console.WriteLine($"Account ID: {i.Id} {i.Name} Balance: {i.Balance} {i.Currency}");
             }
-            int.TryParse(Console.ReadLine(), out int idDebit);
-            int.TryParse(Console.ReadLine(), out int idKredit);
+            int idRemoveFundsHolder = 0;
+            int idAddFundsHolder = 0;
+            float balanceHolder = 0;
+            string idNameFrom = "";
+            string idNameTo = "";
+            string idNameFromCurrency = "";
+            string idNameToCurrency = "";
+
+            Console.WriteLine("Please typ in the ID of the account that you want to transfer funds from.");
+            int.TryParse(Console.ReadLine(), out int idRemoveFunds);
+
+            Console.WriteLine("Please typ in the ID of the account that you want to transfer funds to.");
+            int.TryParse(Console.ReadLine(), out int idAddFunds);
+
+            Console.WriteLine("How much do you want to transfer? Use only digits please.");
             float.TryParse(Console.ReadLine(), out float result);
 
-            foreach (var debit in accountLists) 
+            Console.Clear();
+
+            foreach (var holder in accountLists) 
             {
-                if (debit.Id == idDebit) 
+                if (holder.Id == idRemoveFunds) 
                 {
-                    debit.Balance = debit.Balance - result;
+                    idNameFrom = holder.Name;
+                    balanceHolder = holder.Balance;
+                    idRemoveFundsHolder = holder.Id;
+                    idNameFromCurrency = holder.Currency;
+                }
+                else if (holder.Id == idAddFunds)
+                {
+                    idNameTo = holder.Name;
+                    idAddFundsHolder = holder.Id;
+                    idNameToCurrency = holder.Currency;
                 }
             }
 
-            foreach (var kredit in accountLists)
+            if (idRemoveFundsHolder == idRemoveFunds && idAddFundsHolder == idAddFunds && balanceHolder >= result)
             {
-
-                if (kredit.Id == idKredit)
+                foreach (var funds in accountLists)
                 {
-                    
-                    kredit.Balance = kredit.Balance + result;
+                    if (idRemoveFunds == funds.Id)
+                    {
+                        
+                            funds.Balance = funds.Balance - result;
+                        
+                    }
+                    else if (idAddFunds == funds.Id)
+                    {
+                        
+                            funds.Balance = funds.Balance + result;
+                        
+                    }
                 }
+                accountHistory.Add($"Transaction succesful! From Account name: {idNameFrom} Funds: -{result} {idNameFromCurrency}. To Account name: {idNameTo} Funds: +{result} {idNameToCurrency} {DateTime.Now}");
+            }
+            else 
+            {
+                Console.WriteLine("Could not make your request. Please check if you got valid funds and/or that you typed in the right ID.");
 
+                accountHistory.Add($"Transaction unsuccesful! {DateTime.Now}");
             }
         }
         public void TransactionToUser()
         {
             foreach (var i in accountLists)
             {
-
                 Console.WriteLine($"Account Name: {i.Name}");
             }
 
@@ -109,10 +147,8 @@ namespace CyberCoyotesBank
 
             foreach (var i in accountLists)
             {
-                 
                 if (i.Name == userInputName)
                 {
-                    
                     i.Balance = i.Balance + result;
                 }
             }
@@ -120,14 +156,18 @@ namespace CyberCoyotesBank
         public void CreateAccount(string name, float balance, string currency) 
         {
             accountLists.Add(new Account(Id++, name, currency, balance));
+            accountHistory.Add($"Account created! {DateTime.Now}");
         }
         public void Loan() 
         { 
             
         }
         public void AccountHistory() 
-        { 
-            
+        {
+            foreach (var historyCheck in accountHistory)
+            {
+                Console.WriteLine(historyCheck);
+            }
         }
     }
 }
