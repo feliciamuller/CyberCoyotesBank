03e84d8c8bc8564320b00c0bd5e963118b0796b4
jonnaagus <jonnaagustafssoon@hotmail.com>
2023-11-16 12:55:19 +0100

    transaktionsklasser, var 15:e min

---
+++ Transaction15min.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CyberCoyotesBank
+{
+    internal class Transaction15min
+    {
+        private List<Transaction> pendingTransactions;
+        private System.Timers.Timer timer;
+
+        public Transaction15min()
+        {
+            pendingTransactions = new List<Transaction>();
+            timer = new System.Timers.Timer(15 * 60 * 1000); // 15 minuter i millisekunden
+            timer.Elapsed += OnTimerElapsed;
+            timer.Start();
+        }
+
+        public void ScheduleTransaction(Transaction transaction)
+        {
+            pendingTransactions.Add(transaction);
+        }
+
+        public void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // Transaktionslogik som körs varje 15:e minut
+            Console.WriteLine($"Utför transaktioner kl {DateTime.Now}");
+            foreach (var transaction in pendingTransactions)
+            {
+                transaction.Execute();
+            }
+
+            // Rensa den planerade transaktionslistan
+            pendingTransactions.Clear();
+        }
+    }
+}
