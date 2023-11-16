6d525f266f42777bff6026db25a7f0262b9c32a6
Tobias <92667455+Saibot155@users.noreply.github.com>
2023-11-16 09:22:35 +0100

    removed code to test from program

--- Program.cs
+++ Program.cs
@@ -7,44 +7,8 @@ namespace CyberCoyotesBank
     {
         static void Main(string[] args)
         {
-           // Menu.PrintLogin();
-
-            TESTNEWUSER();
+           Menu.PrintLogin();
         }
 
-        private static void TESTNEWUSER()
-        {
-            Console.WriteLine("new username: ");
-            string usern = Console.ReadLine();
-
-            Console.WriteLine("new password: ");
-            string pass = Console.ReadLine();
-
-            UserManager.CreateUser(usern, pass);
-
-            foreach (User user in UserManager.Users)
-            {
-                Console.WriteLine($"Username: {user.UserName}");
-                Console.WriteLine($"Password: {user.Password}");
-                Console.WriteLine($"ID:{user.ID} ");
-                Console.WriteLine("****************************");
-            }
-
-            Console.WriteLine("new username: ");
-             usern = Console.ReadLine();
-
-            Console.WriteLine("new password: ");
-             pass = Console.ReadLine();
-
-            UserManager.CreateUser(usern, pass);
-
-            foreach (User user in UserManager.Users)
-            {
-                Console.WriteLine($"Username: {user.UserName}");
-                Console.WriteLine($"Password: {user.Password}");
-                Console.WriteLine($"ID:{user.ID} ");
-                Console.WriteLine("****************************");
-            }
-        }
     }
 }
